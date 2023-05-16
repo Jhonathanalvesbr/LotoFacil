ff8d810317a9cd8e15135c84e9ae5943323bfb11
Jhonathanalvesbr <jhonathanalves_br@hotmail.com>
2023-05-16 19:39:44 -0300

    update

--- frontend/src/components/numeros/index.tsx
+++ frontend/src/components/numeros/index.tsx
@@ -22,7 +22,7 @@ function Numeros() {
     });
 
     useEffect(() => {
-        axios.defaults.baseURL = 'https://4qcf0tsa9l.execute-api.us-east-1.amazonaws.com/';
+        axios.defaults.baseURL = 'https://jk03cyy7kk.execute-api.us-east-1.amazonaws.com/';
         axios.get("run")
             .then(r => {
                 for (var i = 0; i < r.data.length; i++) {
