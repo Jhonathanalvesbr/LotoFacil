26d0a522d48a097ce30ef69bad07f3b1ff65e00c
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2024-02-11 22:16:38 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Numeros from './components/numeros'
 
+
+
 function App() {
   return (
     <>
-      <Numeros/>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bem-vindo ao Nosso Site!</h1>
+      <p>Estamos em desenvolvimento, obrigado pela sua paciência.</p>
+    </div>
     </>
   )
 }
